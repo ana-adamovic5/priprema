3af29cd4aa414d4be99128b59654fa5ecd703d0f
ana-adamovic <aa20180201@student.fon.bg.ac.rs>
2023-04-13 09:12:02 +0200

    Napravljen projekat

---
+++ src/main/java/rs/np/adamovic_ana/App.java
@@ -0,0 +1,13 @@
+package rs.np.adamovic_ana;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
